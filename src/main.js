7fea4bbc0573a51588e57bfc1b8d06e21ddd5909
manuch <chingayloez4@gmail.com>
2025-04-15 23:38:08 -0500

    Added Basics styles and funcionality

--- src/main.js
+++ src/main.js
@@ -1,6 +1,7 @@
 const URL = 'https://api.jikan.moe/v4';
 const imagePlace = document.querySelector('.image-place')
 const generatorButton = document.querySelector('.generator-button')
+const imageSection = document.querySelector('.image-section');
 
 async function callApi(urlAPI) {
     try {
@@ -10,7 +11,9 @@ async function callApi(urlAPI) {
 
         if(response.ok) {
             const data = await response.json();
-            imagePlace.src = data.data.images.webp.large_image_url;
+            const objImg = data.data.images.webp.large_image_url;
+            hiddePreviousContainer();
+            getImage(objImg);
         } else {
             setTimeout(() => {
                 callApi(URL);
@@ -26,6 +29,37 @@ function randomImageGenerator() {
     const NUMBER = 60000;
     const randomNumber = Math.floor(Math.random() * NUMBER);
     return randomNumber
+}
+
+function loadingImage() {
+    const div = document.createElement('div');
+    const span = document.createElement('span');
+    const loadingIcon = document.createElement('img');
+    span.textContent = 'Loading Anime...';
+    loadingIcon.src = `./assets/images/270-ring-with-bg.svg`;
+
+    div.append(span, loadingIcon);
+    div.classList.add('loading-image', 'image-section__basic');
+
+    imageSection.append(div);
+}
+
+function getImage(imgSrc) {
+    const div = document.createElement('div');
+    const img = document.createElement('img');
+
+    div.classList.add('anime-img-ok', 'image-section__basic');
+    img.alt = 'Random Anime Image';
+    img.src = imgSrc;
+    div.append(img)
+
+    imageSection.append(div);
+}
+
+function hiddePreviousContainer() {
+    const previousElement = imageSection.firstElementChild;
+    console.log(previousElement)
+    previousElement.style.display = 'none';
 }
 
 generatorButton.addEventListener('click', () => {
